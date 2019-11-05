184c248acf73362a896677e001112da26b5468cb
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-11-05 13:00:07 +0100

    add normalize move vector

--- src/systems/sys_control_ball.ts
+++ src/systems/sys_control_ball.ts
@@ -1,6 +1,7 @@
 import {Get, Has} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
 import {Vec2} from "../math/index.js";
+import {normalize} from "../math/vec2.js";
 
 const QUERY = Has.Transform2D | Has.ControlBall;
 
@@ -14,8 +15,8 @@ export function sys_control_ball(game: Game, delta: number) {
 
 function update(game: Game, entity: Entity, delta: number) {
     let direction = <Vec2>[1, 1];
-
-    let speed = 500;
+    normalize(direction, direction);
+    let speed = 50;
     let transform = game[Get.Transform2D][entity];
     transform.Translation[0] += direction[0] * speed * delta;
     transform.Translation[1] += direction[1] * speed * delta;
